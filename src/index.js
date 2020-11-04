a43956fd36aa9e62029459f74b1e40998b941807
Steven A <s.dailliez@gmail.com>
2020-11-04 15:05:44 +0100

    Add redux tools & prepare root reducers

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore } from 'redux'
+import { createStore, combineReducers ,compose } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './store/reducer'
 
-const store = createStore(reducer)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducer = combineReducers({
+  reducer
+})
+
+const store = createStore(rootReducer, composeEnhancers())
 
 ReactDOM.render(
   <Provider store={store}>
