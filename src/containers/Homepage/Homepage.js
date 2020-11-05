89b6fb1c8fddce628435a6d4f6641033df1acd5a
Steven A <s.dailliez@gmail.com>
2020-11-05 16:46:43 +0100

    Add background for each tile

--- src/containers/Homepage/Homepage.js
+++ src/containers/Homepage/Homepage.js
@@ -6,12 +6,15 @@ const Homepage = () => (
     <Aux>
         <div className="home">
             <div className="card-iss">
+                <div className="bg-iss"></div>
                 <p className="card-title">Test</p>
             </div>
             <div className="card-astro">
+                <div className="bg-astro"></div>
                 <p className="card-title">Test 2</p>
             </div>
             <div className="card-rover">
+                <div className="bg-rover"></div>
                 <p className="card-title">Test 3</p>
             </div>
         </div>
