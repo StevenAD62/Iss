fd3b265a0df0b66f8e944a7a3715c4f6720ab46a
Steven A <s.dailliez@gmail.com>
2020-11-07 21:56:08 +0100

    Display a satellite on map

--- src/containers/ISS/Iss.js
+++ src/containers/ISS/Iss.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
 import Aux from '../../hoc/Aux'
 import './Iss.css'
-import {MapContainer, Marker, Popup, TileLayer} from 'react-leaflet'
+import {MapContainer, Marker, TileLayer} from 'react-leaflet'
 import { Icon } from 'leaflet'
 
 
 const Iss = () => {
+    const [marker, setMarker] = useState([])
+
+    const satelliteIcon = new Icon ({
+        iconUrl: '/satellite.svg',
+        iconSize: [45,45]
+    })
+
+    const fetching = async () => {
+        const results = await fetch('http://api.open-notify.org/iss-now.json')
+        const data = await results.json()
+        console.log('DATA , ', data)
+        setMarker([Number(data.iss_position.latitude), Number(data.iss_position.longitude)])
+    }
+
+    useEffect(() => {
+        if (marker.length === 0) {
+            fetching()
+        } else {
+        const interval = setInterval(() => {
+           fetching()
+        }, 5000);
+        return () => clearInterval(interval)}
+    }, [marker])
+
     return (
         <Aux>
             <div className="map-container">
@@ -13,6 +37,10 @@ const Iss = () => {
                     <TileLayer
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker 
+                        position={marker.length ? [marker[0], marker[1]] : [1,1]}
+                        icon={satelliteIcon}
                     />
                 </MapContainer>
             </div>
