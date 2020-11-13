d82d31ea6d5bb2a26fd2cf1e2730d634ccda9b2b
Steven A <s.dailliez@gmail.com>
2020-11-13 11:30:13 +0100

    Add loading when the date is modified

--- src/containers/Space/Space.js
+++ src/containers/Space/Space.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react'
-import Calendar from '../../UI/Calendar'
+import Calendar from '../../UI/Calendar/Calendar'
+import Spinner from '../../UI/Spinner/Spinner'
 import './Space.css'
 
 const Space = () => {
     const [picture, setPicture] = useState({})
-    const [dateData, setDateData] = useState({})
+    const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
         fetch('https://api.nasa.gov/planetary/apod?api_key=' + process.env.REACT_APP_NASA_API_KEY)
@@ -15,12 +16,21 @@ const Space = () => {
     }, [])
 
     const dateHandler = (value) => {
+        setIsLoading(true)
+        console.log('VALUE => ', value)
         const data = {
-            day: value.getDay(),
-            month: value.getMonth(),
-            year: value.getYear()
+            day: value.getDate() +1,
+            month: value.getMonth() + 1,
+            year: value.getYear() + 1900
         }
-        setDateData(data)
+        console.log('DATE OBJECT => ', data)
+        fetch(`https://api.nasa.gov/planetary/apod?date=${data.year}-${data.month}-${data.day}&api_key=` + process.env.REACT_APP_NASA_API_KEY)
+            .then(response => response.json())
+            .then(resData => {
+                console.log(resData)
+                setPicture(resData)
+                setIsLoading(false)
+            })
     }
 
     return (
@@ -29,14 +39,22 @@ const Space = () => {
                 <p>Choose a date and see the astronomic picture :</p>
                 <Calendar onChange={(value) => dateHandler(value)} />
             </div>
-            <div className='picture-container'>
-                <img className='space-picture' src={picture.url} alt={picture.title} />
-                <div className='details-container'>
-                    <p className='space-title'>{picture.title}</p>
-                    <p className='space-author'><em>{picture.copyright}</em></p>
-                    <p className='space-description'>{picture.explanation}</p>
+            {isLoading
+                ? (<div className='spinner-container'><Spinner /></div>)
+                : (
+                    <div className='picture-container'>
+                    {picture.media_type === 'video'
+                        ? (<iframe className='space-picture' title={picture.tile} src={picture.url}></iframe>)
+                        : (<img className='space-picture' src={picture.url} alt={picture.title} />)
+                    }
+                    <div className='details-container'>
+                        <p className='space-title'>{picture.title}</p>
+                        <p className='space-author'><em>{picture.copyright}</em></p>
+                        <p className='space-description'>{picture.explanation}</p>
+                    </div>
                 </div>
-            </div>
+                )
+            }
         </div>
     )
 };
